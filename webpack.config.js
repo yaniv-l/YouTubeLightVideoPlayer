27e8c1c439142830d9b8997778c9a0f34cee2449
yaniv-l <yanivl@srl.co.il>
2016-08-25 14:39:36 +0300

    fixed package.json for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,10 @@ module.exports = {
       }
     }]
   },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
   resolve: {
-    modulesDirectories: ['node_modules', 'src'],
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
